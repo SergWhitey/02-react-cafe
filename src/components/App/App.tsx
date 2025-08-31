75e18358e4ec944ad2757d4fb384881bfd84070d
SergWhitey <truesteell@gmail.com>
2025-08-31 16:31:54 +0300

    f3

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import CafeInfo from "../CafeInfo/CafeInfo";
 import Notification from "../Notification/Notification";
-import type { Votes, VoteType } from '../../types/Votes';
+import type { Votes, VoteType } from '../../types/votes';
 import VoteOptions from "../VoteOptions/VoteOptions";
 import VoteStats from "../VoteStats/VoteStats";
 import css from "./App.module.css"
